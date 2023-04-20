7ab1dee04903e4cd6899f912e553848b376faee1
FajryAriansyah <fajry@mhs.unsyiah.ac.id>
2023-04-20 16:38:25 +0700

    redesign UI

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,6 +10,7 @@ class PostController extends Controller
     public function index(){
         return view('posts',[
             "title" => "All Posts",
+            "active" => 'posts',
             "posts" => Post::latest()->get() // agar yang terbaru lebih dulu
         ]);  
     }
@@ -17,6 +18,7 @@ class PostController extends Controller
     public function show(Post $post){
         return view('post', [
             "title" => "Single Post",
+            "active" => 'posts',
             "post" => $post
         ]);
     }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,12 +1,26 @@
 @extends('layouts.main')
 
 @section('container')
-    <h2 class="mb-5">{{ $post->title }}</h2>
-    
-    <p>By. <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none"> {{ $post->category->name }}</a></p>
-    
-    {!! $post->body !!} {{-- Agar dapat menampilkan tag HTML --}}
 
+    <div class="container">
+        <div class="row justify-content-center mb-5">
+            <div class="col-md-8">
+                <h2 class="mb-3">{{ $post->title }}</h2>
+                <p>By. <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none"> {{ $post->category->name }}</a></p>
+                
+                <img src="https://source.unsplash.com/1200x400/?{{ $post->category->name }}" alt="{{ $post->category->name }}" class="img-fluid">
 
-    <a href="/posts" class="d-block mt-3 text-decoration-none">Back to Posts</a>
+                <article class="my-3 fs-5">
+                    {!! $post->body !!} {{-- Agar dapat menampilkan tag HTML --}}
+                    
+                </article>
+            
+
+                <a href="/posts" class="d-block mt-3 text-decoration-none">Back to Posts</a>
+                
+            </div>
+        </div>
+    </div>
+
+    
 @endsection
